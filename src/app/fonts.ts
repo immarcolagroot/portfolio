9c01c19c8ef8252f4ea51f3480a5cbda9818238e
João Marcolino <otalmarcolino@gmail.com>
2023-11-30 16:07:45 -0300

    feat: create default file of export fonts used

---
+++ src/app/fonts.ts
@@ -0,0 +1,5 @@
+import { Heebo, Kalam } from "next/font/google";
+
+export const heebo = Heebo({ weight: ["400", "500"], subsets: ["latin"] });
+
+export const kalam = Kalam({ weight: "400", subsets: ["latin"] });
